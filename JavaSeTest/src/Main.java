136633ba0b90fb1389c6727958e69d0a36dc1f28
github-shanti <35558390+github-shanti@users.noreply.github.com>
2019-10-02 22:35:03 +0800

    Revert "just add one sentence"
    This reverts commit 984f8337dd4d73ce651609f48ee8c59ccb7b2c44.

--- JavaSeTest/src/Main.java
+++ JavaSeTest/src/Main.java
@@ -2,11 +2,9 @@
 public class Main {
 	
 	public static void main(String[] args) {
-		String b = "老子啥也没干，就加了一行代码！" ;
 		String a="这是一个a";
-		
+		int b =33 ;
 		String c="后来在master上加的一个c";
-		
 
 	}
 
